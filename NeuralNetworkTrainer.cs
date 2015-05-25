e4a808986c613fb691b7d75401090059113b1022
jnickg <jnickg83@gmail.com>
2015-05-25 01:49:18 -0700

    Add NN trainer beginnings

---
+++ NeuralNetworkTrainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacTortex
+{
+    public struct TrainingSet
+    {
+        public Dictionary<double[], double[]>  IOMappings;
+        public ActivationFunction f;
+    }
+
+    public class NeuralNetworkTrainer
+    {
+        private NeuralNetwork nn;
+        private TrainingSet ts;
+
+        public NeuralNetworkTrainer(ref NeuralNetwork nn, TrainingSet ts)
+        {
+            this.nn = nn;
+            this.ts = ts;
+        }
+
+        public void Train(int repetitions)
+        {
+            nn.Initialize(ts.f);
+            for (int i = 0; i < repetitions; ++i)
+            {
+                // Loop through all examples
+                foreach (var kvp in ts.IOMappings)
+                {
+                    // Key is Input; Value is Output
+                    
+                    // Train the neural network
+                }
+            }
+        }
+    }
+}
