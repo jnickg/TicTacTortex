dda448b38b4c62e95c4e0da4d2c5948b51bfebd8
jnickg <jnickg83@gmail.com>
2015-05-26 21:31:31 -0700

    Add TicTacToe game that allows saving of game histories.
    Used to train TicTacTortex.

---
+++ TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_restart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.richTextBox_moves = new System.Windows.Forms.RichTextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_restart
+            // 
+            this.button_restart.Location = new System.Drawing.Point(13, 13);
+            this.button_restart.Name = "button_restart";
+            this.button_restart.Size = new System.Drawing.Size(237, 23);
+            this.button_restart.TabIndex = 0;
+            this.button_restart.Text = "Restart";
+            this.button_restart.UseVisualStyleBackColor = true;
+            this.button_restart.Click += new System.EventHandler(this.button_restart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 61);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(94, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 61);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(175, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 61);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(13, 109);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 61);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(94, 109);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 61);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(175, 109);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 61);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(13, 176);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 61);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(94, 176);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 61);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(175, 176);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 61);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // richTextBox_moves
+            // 
+            this.richTextBox_moves.Enabled = false;
+            this.richTextBox_moves.Location = new System.Drawing.Point(12, 243);
+            this.richTextBox_moves.Name = "richTextBox_moves";
+            this.richTextBox_moves.Size = new System.Drawing.Size(238, 137);
+            this.richTextBox_moves.TabIndex = 10;
+            this.richTextBox_moves.Text = "";
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(94, 386);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 11;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 430);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.richTextBox_moves);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_restart);
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_restart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RichTextBox richTextBox_moves;
+        private System.Windows.Forms.Button button_save;
+    }
+}
+
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        bool isXTurn = true;
+        List<Button> buttons;
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+            buttons = new List<Button>();
+            buttons.Add(button1);
+            buttons.Add(button2);
+            buttons.Add(button3);
+            buttons.Add(button4);
+            buttons.Add(button5);
+            buttons.Add(button6);
+            buttons.Add(button7);
+            buttons.Add(button8);
+            buttons.Add(button9);
+            newGame();
+        }
+
+        private void newGame()
+        {
+            foreach (Button b in buttons)
+            {
+                b.Text = "";
+                b.Enabled = true;
+            }
+            isXTurn = true;
+            richTextBox_moves.AppendText("N\n");
+        }
+
+        private void button_restart_Click(object sender, EventArgs e)
+        {
+            newGame();
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileStream fs = new FileStream(sfd.FileName, FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(richTextBox_moves.Text);
+                sw.Close();
+                fs.Close();
+            }
+        }
+
+        private void executeTurn(Button b)
+        {
+            b.Text = isXTurn ? "X" : "O";
+            b.Enabled = false;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{");
+            foreach (Button but in buttons)
+            {
+                sb.Append(but.Text == "X" ? 1 : 0);
+            }
+            sb.Append("}{");
+            foreach (Button but in buttons)
+            {
+                sb.Append(but.Text == "O" ? 1 : 0);
+            }
+            sb.Append("}\n");
+            richTextBox_moves.AppendText(sb.ToString());
+
+            isXTurn = !isXTurn;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[0]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[1]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[2]);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[3]);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[4]);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[5]);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[6]);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[7]);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            executeTurn(buttons[8]);
+        }
+    }
+}
