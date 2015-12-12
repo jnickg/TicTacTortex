0571526c0daac91ce9f6fe994ba85373bc436faa
jnickg <jnickg83@gmail.com>
2015-12-11 19:02:19 -0800

    wtfidk

--- Program.cs
+++ Program.cs
@@ -14,6 +14,17 @@ namespace TicTacTortex
         {
             NeuralNetwork nn = new NeuralNetwork(64, 128, 64);
             nn.Initialize(ActivationFunction.Step);
+            TrainingSet ts = new TrainingSet();
+
+            StreamReader sr = new StreamReader(args[0]);
+
+            while (!sr.EndOfStream)
+            {
+                string ioString = sr.ReadLine();
+                double[] inputs = new double[9];
+                double[] outputs = new double[9];
+                ts.IOMappings.Add(inputs, outputs);
+            }
         }
     }
 }
